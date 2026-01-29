f9309de942c901e3dad07f78899f77238e32733e
Júlia Campolim <julia.campolim@gmail.com>
2026-01-29 15:51:03 -0300

    feat: project 1

---
+++ WeSlipt/WeSlipt/WeSliptApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSliptApp.swift
+//  WeSlipt
+//
+//  Created by Júlia Campolim de Oste on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSliptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
